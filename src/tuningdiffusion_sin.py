47059e9078a793fd1586b7d4d0100f55b7dc474c
VitorSamuel Brito <vitorsamuelbr@gmail.com>
2024-10-11 20:27:29 -0300

    restructuring of functions

--- src/tuningdiffusion_sin.py
+++ src/tuningdiffusion_sin.py
@@ -1,7 +1,60 @@
+import numpy as np
+
+#-----------------------
+
 #| ### Documentation: https://numpy.org/doc/stable/reference/index.html , https://numpy.org/doc/stable/reference/generated/numpy.savetxt.html, https://docs.python.org/3/library/random.html
 #| #### Instructions: This code generates diffusive trajectories that represent a folding of a protein.
 
-import numpy as np
+#-----------------------
+## 
+
+def DD(DIFFX, SINM, X, SINF):
+    eqd = DIFFX+SINM*np.sin(X/SINF)
+    return eqd
+
+def DDslope(DIFFX, SINM, X, SINF):
+    eqs = SINM/SINF*np.cos(X/SINF)
+    return eqs
+
+def grad24(M, D, HEIGHT, X):
+    eq1 = (-2*HEIGHT*2*(X-M)/D**2 +4*HEIGHT*(X-M)**3/D**4)
+    return eq1
+
+def E24(M, D, HEIGHT, X):
+    eq2 = (-HEIGHT*2*(X-M)**2/D**2 +HEIGHT*(X-M)**4/D**4)
+    return eq2
+    
+def gradG(Max, sigma, HEIGHT, X):
+    #Sg = [l**2 for l in sigma]
+    eq3 = HEIGHT*np.exp(-(X-Max)**2/sigma**2)*2*(Max-X)/sigma**2 
+    return eq3
+    
+def EG(Max, sigma, HEIGHT, X):
+    #Sg = [l**2 for l in sigma]
+    eq4 = HEIGHT*np.exp(-(X-Max)**2/sigma**2)
+    return eq4
+
+#print(DD(DIFFX, SINM, X, SINF), DDslope(DIFFX, SINM, X, SINF), grad24(M, D, HEIGHT, X), E24(M, D, HEIGHT, X), gradG(Max, sigma, HEIGHT, X), EG(Max, sigma, HEIGHT, X))
+
+
+def gaussian (DIFFX, dt):
+    # sd is the rms value of the distribution.
+    sd = np.sqrt(2*DIFFX*dt)
+    RR = 0 
+    while True:
+        M1 = np.random.random()
+        M2 = np.random.random()
+        M1 = 2*(M1-0.5)
+        M2 = 2*(M2-0.5)
+        tmp1 = M1**2 + M2**2
+        if tmp1 <= 1.0 and tmp1 >= 0.0:
+            tmp2 = sd*np.sqrt( -2*np.log(tmp1)/tmp1 )
+            RR = M1*tmp2
+            break
+    return RR
+
+#print(gaussian(DIFFX, dt))
+
 
 #-----------------------
 
@@ -75,61 +128,6 @@ for i in range(int(NG)):
 # print(Max)
 # print(sigma)
 # print(GH)
-
-#-----------------------
-
-###
-
-def DD(DIFFX, SINM, X, SINF):
-    eqd = DIFFX+SINM*np.sin(X/SINF)
-    return eqd
-
-def DDslope(DIFFX, SINM, X, SINF):
-    eqs = SINM/SINF*np.cos(X/SINF)
-    return eqs
-
-def grad24(M, D, HEIGHT, X):
-    eq1 = (-2*HEIGHT*2*(X-M)/D**2 +4*HEIGHT*(X-M)**3/D**4)
-    return eq1
-
-def E24(M, D, HEIGHT, X):
-    eq2 = (-HEIGHT*2*(X-M)**2/D**2 +HEIGHT*(X-M)**4/D**4)
-    return eq2
-    
-def gradG(Max, sigma, HEIGHT, X):
-    #Sg = [l**2 for l in sigma]
-    eq3 = HEIGHT*np.exp(-(X-Max)**2/sigma**2)*2*(Max-X)/sigma**2 
-    return eq3
-    
-def EG(Max, sigma, HEIGHT, X):
-    #Sg = [l**2 for l in sigma]
-    eq4 = HEIGHT*np.exp(-(X-Max)**2/sigma**2)
-    return eq4
-
- 
-#print(DD(DIFFX, SINM, X, SINF), DDslope(DIFFX, SINM, X, SINF), grad24(M, D, HEIGHT, X), E24(M, D, HEIGHT, X), gradG(Max, sigma, HEIGHT, X), EG(Max, sigma, HEIGHT, X))
-
-#-----------------------
-
-###
-
-def gaussian (DIFFX, dt):
-    # sd is the rms value of the distribution.
-    sd = np.sqrt(2*DIFFX*dt)
-    RR = 0 
-    while True:
-        M1 = np.random.random()
-        M2 = np.random.random()
-        M1 = 2*(M1-0.5)
-        M2 = 2*(M2-0.5)
-        tmp1 = M1**2 + M2**2
-        if tmp1 <= 1.0 and tmp1 >= 0.0:
-            tmp2 = sd*np.sqrt( -2*np.log(tmp1)/tmp1 )
-            RR = M1*tmp2
-            break
-    return RR
-
-#print(gaussian(DIFFX, dt))
 
 #-----------------------
 
