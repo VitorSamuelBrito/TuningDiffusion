00c16b0b44028b8ab0ebaccfea44fc6b9df274ef
Vitor Brito <you@example.com>
2024-12-20 19:11:27 -0300

    add others functions in script test_functions

--- share/test_library_diffusion.py
+++ share/test_library_diffusion.py
@@ -67,6 +67,86 @@ def cartesian(arrays, out=None):
 
 
 ## comparing each function with respective perl results
+def test_Vx():
+    """Function to test the Vx function from library_diffusion"""
+    ## To be used in all comparisons
+    threshold = 0.001
+    ## loading the sequence used to generate data
+    sequence = np.genfromtxt('share/sequence.dat')
+    ## generate the vector used as input
+    input = cartesian([sequence, sequence, sequence, sequence])
+
+    # loading the data from DDsin generated with perl
+    grad24_data = np.genfromtxt('share/grad24_test.dat')
+    # calculating the results
+    results = []
+    for a, b, c, d in input:
+        results.append([a, b, c, d, libdiff.Vx(a, b, c, d)])
+    results = np.asarray(results)
+    test = np.less_equal(np.absolute(np.subtract(results, grad24_data)), \
+                         threshold).all()
+    assert test
+
+def test_Fx():
+    """Function to test the Fx function from library_diffusion"""
+    ## To be used in all comparisons
+    threshold = 0.001
+    ## loading the sequence used to generate data
+    sequence = np.genfromtxt('share/sequence.dat')
+    ## generate the vector used as input
+    input = cartesian([sequence, sequence, sequence, sequence])
+
+    # loading the data from DDsin generated with perl
+    E24_data = np.genfromtxt('share/E24_test.dat')
+    # calculating the results
+    results = []
+    for a, b, c, d in input:
+        results.append([a, b, c, d, libdiff.Fx(a, b, c, d)])
+    results = np.asarray(results)
+    test = np.less_equal(np.absolute(np.subtract(results, E24_data)), \
+                         threshold).all()
+    assert test
+
+def test_VG():
+    """Function to test the VG function from library_diffusion"""
+    ## To be used in all comparisons
+    threshold = 0.001
+    ## loading the sequence used to generate data
+    sequence = np.genfromtxt('share/sequence.dat')
+    ## generate the vector used as input
+    input = cartesian([sequence, sequence, sequence, sequence])
+
+    # loading the data from DDsin generated with perl
+    gradG_data = np.genfromtxt('share/gradG_test.dat')
+    # calculating the results
+    results = []
+    for a, b, c, d in input:
+        results.append([a, b, c, d, libdiff.VG(a, b, c, d)])
+    results = np.asarray(results)
+    test = np.less_equal(np.absolute(np.subtract(results, gradG_data)), \
+                         threshold).all()
+    assert test
+
+def test_FG():
+    """Function to test the FG function from library_diffusion"""
+    ## To be used in all comparisons
+    threshold = 0.001
+    ## loading the sequence used to generate data
+    sequence = np.genfromtxt('share/sequence.dat')
+    ## generate the vector used as input
+    input = cartesian([sequence, sequence, sequence, sequence])
+
+    # loading the data from DDsin generated with perl
+    EG_data = np.genfromtxt('share/EG_test.dat')
+    # calculating the results
+    results = []
+    for a, b, c, d in input:
+        results.append([a, b, c, d, libdiff.FG(a, b, c, d)])
+    results = np.asarray(results)
+    test = np.less_equal(np.absolute(np.subtract(results, EG_data)), \
+                         threshold).all()
+    assert test
+
 def test_Dxsin():
     """Function to test the Dxsin function from library_diffusion"""
     ## To be used in all comparisons
@@ -84,5 +164,25 @@ def test_Dxsin():
         results.append([a, b, c, d, libdiff.Dxsin(a, b, c, d)])
     results = np.asarray(results)
     test = np.less_equal(np.absolute(np.subtract(results, DDsin_data)), \
+                         threshold).all()
+    assert test
+
+def test_Dxsinpartial():
+    """Function to test the Dxsinpartial function from library_diffusion"""
+    ## To be used in all comparisons
+    threshold = 0.001
+    ## loading the sequence used to generate data
+    sequence = np.genfromtxt('share/sequence.dat')
+    ## generate the vector used as input
+    input = cartesian([sequence, sequence, sequence, sequence])
+
+    # loading the data from DDsin generated with perl
+    DDsinslope_data = np.genfromtxt('share/DDsinslope_test.dat')
+    # calculating the results
+    results = []
+    for a, b, c, d in input:
+        results.append([a, b, c, d, libdiff.Dxsinpartial(a, b, c, d)])
+    results = np.asarray(results)
+    test = np.less_equal(np.absolute(np.subtract(results, DDsinslope_data)), \
                          threshold).all()
     assert test
