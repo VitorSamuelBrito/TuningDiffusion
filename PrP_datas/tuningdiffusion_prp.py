451f9d1deea9206a2f23b19eaee680f8a61f2e02
VitorSamuel Brito <vitorsamuelbr@gmail.com>
2025-09-26 20:33:03 -0300

    generating new trajectory with dq pol

--- PrP_datas/tuningdiffusion_prp.py
+++ PrP_datas/tuningdiffusion_prp.py
@@ -20,8 +20,8 @@ data_y = data[:,1]
 
 #-----------------------
 ## Initial parameters
-coef_init = np.polyfit(energy_x, energy_y, 4) 
-coef_init_new = np.polyfit(data_x, data_y, 3) 
+coef_init_free = np.polyfit(energy_x, energy_y, 4) 
+coef_init_dq = np.polyfit(data_x, data_y, 3) 
 # A0 = (data_y.max()-data_y.min()) * (data_x.max()-data_x.min())
 # loc0 = data_x[np.argmax(data_y)]
 # beta0 = (data_x.max()-data_x.min())/8
@@ -30,14 +30,14 @@ coef_init_new = np.polyfit(data_x, data_y, 3)
 STEPS = 1000000
 dt = 0.01
 
-p0_pol = coef_init[::-1]  
-p0_pol_new = coef_init_new[::-1] 
+p0_pol_free = coef_init_free[::-1]  
+p0_pol_dq = coef_init_dq[::-1] 
 # p0_g = [A0, loc0, beta0, C0]                                      
 
 #-----------------------
 ## Curve fitting
-popp, pcop = curve_fit(libdiff.quartic_model, energy_x, energy_y, p0=p0_pol, maxfev=10000)
-popt, pcot = curve_fit(libdiff.third_model, data_x, data_y, p0=p0_pol_new, maxfev=10000)
+popp, pcop = curve_fit(libdiff.quartic_model, energy_x, energy_y, p0=p0_pol_free, maxfev=10000)
+popt, pcot = curve_fit(libdiff.third_model, data_x, data_y, p0=p0_pol_dq, maxfev=10000)
 # popg, pcog = curve_fit(
 #     libdiff.gumbel_model, data_x, data_y, p0=p0_g, maxfev=10000,
 #     bounds=([0, data_x.min(), 1e-6, -np.inf],
@@ -51,17 +51,17 @@ fit_DQ_partial = libdiff.third_deriv(data_x, *popt)
 
 #-----------------------
 ## Interpolation
-
 F_interp  = interp1d(energy_x, fit_free, kind='cubic', fill_value="extrapolate")
 Fp_interp = interp1d(energy_x, fit_free_partial, kind='cubic', fill_value="extrapolate")
 D_interp  = interp1d(data_x, fit_DQ, kind='cubic', fill_value="extrapolate")
 Dp_interp = interp1d(data_x, fit_DQ_partial, kind='cubic', fill_value="extrapolate")
 
-V_interp = []
+F_itp  = F_interp(energy_x) 
+Fp_itp = Fp_interp(data_x) 
+D_itp  = D_interp(data_x) 
+Dp_itp = Dp_interp(data_x) 
 
-for k in range(1, 258):
-    drift =  float(Dp_interp(k)) - float(D_interp(k))*float(F_interp(k))
-    V_interp.append(drift)
+V_itp = Dp_itp - D_itp*Fp_itp
 
 #-----------------------
 ## Trajectory calculation
@@ -89,13 +89,12 @@ for i in range(1, STEPS + 1):
 
 Q = np.asarray(Q)
 T = np.asarray(T)
-V_interp = np.asarray(V_interp)
 
 # Save datas 
 traj = np.stack((T, Q), axis=-1)
-DQ =  np.stack((data_x, fit_DQ), axis=-1)
-FQ =  np.stack((energy_x, fit_free), axis=-1)
-VQ =  np.stack((data_x, V_interp), axis=-1)
+DQ =  np.stack((data_x, D_itp), axis=-1)
+FQ =  np.stack((energy_x, F_itp), axis=-1)
+VQ =  np.stack((data_x, V_itp), axis=-1)
 
 np.savetxt("TRAJECTORY_PrP_new_fit.dat", traj, fmt="%12.6f")
 np.savetxt("DQ_PrP_new_fit.dat", DQ, fmt="%12.6f")
@@ -121,8 +120,8 @@ np.savetxt("Drift_PrP_new_fit.dat", VQ, fmt="%12.6f")
 # log_hist_min = log_hist.min()
 # Q_log_hist = log_hist - log_hist_min
 
-# fit_min = fit_free.min()
-# Q_fit_free = fit_free - fit_min
+# fit_min = F_itp.min()
+# Q_fit_free = F_itp - fit_min
 
 # energy_min = energy.min()
 # Q_energy = energy - energy_min # my Q_min force init in zero
@@ -131,31 +130,33 @@ np.savetxt("Drift_PrP_new_fit.dat", VQ, fmt="%12.6f")
 ##  Print grafics to save
 
 # plt.scatter(data_x, data_y, s=0.8, label='DQ', color='black')
-# plt.plot(data_x, fit_DQ, label='DQ fit', color='red')
+# plt.plot(data_x, D_itp, label='DQ fit', color='red')
 # plt.legend()
 # plt.xlabel("reaction coordinate Q")
 # plt.ylabel("DQ")
 # plt.title("Comparative")
 # plt.show() ## pritn DQ
 
-
 # plt.scatter(energy_x, Q_energy, s=0.8, label='Free Energy ajt', color='black')
 # plt.plot(energy_x, Q_fit_free, label='Free Energy fit ajt', color='red')
 # plt.plot(bin_centers, Q_log_hist, 'b--', label='Free Energy hist ajt')
-# # plt.plot(energy_x, k_corr, 'g--', label='Free Energy hist_corr')
 # plt.legend()
 # plt.xlabel("reaction coordinate Q")
 # plt.ylabel("FQ/kT")
 # plt.title("Comparative")
 # plt.show() ## print FQ
 
-# plt.plot(data_x, V_interp, label='VQ', color='black')
+# plt.plot(data_x, V_itp, label='VQ', color='black')
 # plt.legend()
 # plt.xlabel("reaction coordinate Q")
 # plt.ylabel("drift")
 # plt.title("Drift")
 # plt.show() ## pritn VQ
 
-'- Gerar novas trajetorias com novo DQ_fit'
-'- Mudar, passar a salvar DQ e FQ interpolados (teste)'
-'- Salvar gráficos novos interpolados'
+'- Gerar novas trajetorias com novo DQ_fit -  feito'
+'- Mudar, passar a salvar DQ e FQ interpolados - feito'
+'- Salvar gráficos novos interpolados - feito'
+'- Salvar gráficos ajustados e aprox - feito'
+'- Rodar DrDrift com as trajetorias geradas - função polinomial preferência, mais rodar com a gumbel também'
+'- Rodar DrDrift com a trajetoria simulada'
+'- Comparar tempos de passagem e etc'
